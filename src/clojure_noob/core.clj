684dd1f86cbe16bca3210d38073c5292d86664a4
Metin Emiroğlu <amiroff@gmail.com>
2013-09-28 17:13:31 +0300

    Define a function and run a

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -5,3 +5,11 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn too-enthusiastic
+  "Return a cheer that might sound a bit too enthusiastic"
+  [name]
+  (str "OH  MY GOD! " name " YOU ARE BEST MAN SLASH WOMAN "
+   "EVER. WE SHOULD RUN AWAY!"))
+
+(too-enthusiastic "Zelda")
